64dda308f9f4fb271ff58a5216faf05863d9e508
nab <nabil.hayun@gmail.com>
2024-08-11 14:40:24 +0100

    Init Repo

---
+++ Makefile
@@ -0,0 +1,38 @@
+CC = cc
+FLAGS = -Wall -Wextra -Werror -O3
+ENTRY = ./mandatory/cub3d.c
+SRCS =	./mandatory/srcs/init.c
+OBJS = $(SRCS:%.c=%.o)
+OBJS_BON = $(SRCS_BON:%.c=%.o)
+INCLUDE = ./mandatory/cub3d.h
+MLX_MACOS = -framework Cocoa -framework OpenGL -framework IOKit
+MLX_INCLUDE = ./MLX42/include/MLX42.h
+MLX42_DIR = ./MLX42
+GLFW = -lglfw -L/Users/nhayoun/.brew/opt/glfw/lib/
+MLX_AR = ./MLX42/build/libmlx42.a
+MLXLIB = ./MLX42/lib/MLX42
+FSANITIZE= -g -fsanitize=address
+NAME = cub3D
+
+all: $(MLX_AR) $(NAME)
+
+bonus : $(MLX_AR) $(NAME_BON)
+
+$(MLX42_DIR):
+	@echo "Cloning MLX42..."
+	@git clone https://github.com/codam-coding-college/MLX42.git > /dev/null 2>&1
+	@echo "MLX42 has been successfully cloned."
+
+$(MLX_AR): $(MLX42_DIR)
+	@echo "Building MLX42..."
+	@git submodule update --init --recursive
+	@cd MLX42 && cmake -B build && cmake --build build -j4 > /dev/null 2>&1
+	@echo "MLX42 has been successfully built." > /dev/null 2>&1
+
+$(NAME): $(INCLUDE) $(OBJS)
+	make -C mandatory
+clean:
+	make clean -C mandatory
+fclean: clean
+	rm -rf $(NAME) $(NAME_BON)
+re: fclean all
---
+++ mandatory/cub3d.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nhayoun <nhayoun@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/11 12:23:10 by nhayoun           #+#    #+#             */
+/*   Updated: 2024/08/11 13:15:21 by nhayoun          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+# include "../MLX42/include/MLX42/MLX42.h"
+# include <math.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+//# define WIDTH 800
+//# define HEIGHT 800
+
+int	init(void);
+
+#endif
